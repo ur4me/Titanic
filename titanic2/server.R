352e625e6232a85d7de868b17cac6849daf91560
Steve Kim <30827345+ur4me@users.noreply.github.com>
2017-11-06 10:16:30 +1300

    Add files via upload

--- titanic2/server.R
+++ titanic2/server.R
@@ -1,61 +1,38 @@
-library(mice)
+library(gbm)
+library(plyr)
 library(dplyr)
-library(randomForest)
 library(caret)
 library(shiny)
-setwd('c:/Kaggle/titanic')
+
+
 #Combine train.csv and test.csv files
-train <- read.csv('train.csv', na.strings = c("", "NA"), stringsAsFactors = F)
-test <- read.csv('test.csv', na.strings = c("", "NA"), stringsAsFactors = F)
-total <- bind_rows(train, test)
+train1 <- read.csv("./data/cleaned_titanic.csv", na.strings = c("", "NA"), stringsAsFactors = T)
+train1$Pclass <- as.factor(train1$Pclass)
 
 
-# Titles with very low cell counts to be combined to "rare" level
-rare_title <- c('Dona', 'Lady', 'the Countess','Capt', 'Col', 'Don', 
-                'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer')
-#make Title column
-total$Title <- gsub('(.*, )|(\\..*)', '', total$Name)
-total$Title[total$Title == 'Mlle'] <- 'Miss'
-total$Title[total$Title == 'Ms'] <- 'Miss'
-total$Title[total$Title == 'Mme'] <- 'Mrs'
-total$Title[total$Title %in% rare_title]  <- 'Rare Title'
-#make family size column that contains the passenger themselves
-total$FamilySize <- total$SibSp + total$Parch + 1
-#make Deck column
-total$Deck <- factor(sapply(total$Cabin, function(x) strsplit(x, NULL)[[1]][1]))
+modelFit <- train(Survived ~ ., data  = train1, method  = "gbm")
 
-#missing value imputation
-total1 <- total[,-c(1,2,4,9,11)]
-total1 <- as.data.frame(unclass(total1))
-
-#imputing missing values
-miceMod <- mice(total1, method="rf")
-miceOutput <- complete(miceMod)
-
-#separate
-train1 <- miceOutput[1:891,]
-test1 <- miceOutput[892:1309,]
-
-#add Survived column to train1
-Survived <- train$Survived
-train1 <- cbind(train1,Survived)
-train1$Survived <- as.factor(train1$Survived)
-
-#predict with Random Forest
-model_1 <- randomForest(Survived ~ ., data=train1)
-
-predict(model_1, test1)
-
-
-pred_tit <- function(Pclass, Sex, Age, SibSp, Parch, Fare, Embarked, Title, FamilySize, Deck){
-  inputdata <- c(Pclass, Sex, Age, SibSp, Parch, Fare, Embarked, Title, FamilySize, Deck)
-  pred_data <- as.data.frame(t(inputdata))
-  colnames(pred_data) <- c("Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "Title", "FamilySize", "Deck")
-  surv_prob <- predict(model_1,pred_data , type = "response" )
-  return(surv_prob)
-}
-
-shinyServer(
-  function(input, output) {
-    output$prob <- renderText({pred_tit(input$Pclass,input$Sex, input$Age, input$SibSp, input$Parch, input$Fare, input$Embarked, input$Title, input$FamilySize, input$Deck )})
+shinyServer(function(input, output){
+  
+  userdf <- train1[11,]
+  values <- reactiveValues()
+  values$df <- userdf
+  newEntry <- observe({
+    values$df$Pclass <- as.factor(input$Pclass)
+    values$df$Sex <- as.factor(input$Sex)
+    values$df$Age <- as.numeric(input$Age)
+    values$df$SibSp <- as.integer(input$SibSp)
+    values$df$Parch <- as.integer(input$Parch)
+    values$df$Fare <- as.numeric(input$Fare)
+    values$df$Embarked <- as.factor(input$Embarked)
+    values$df$Title <- as.factor(input$Title)
+    values$df$FamilySize <- as.integer(input$FamilySize)
+    values$df$Deck <- as.factor(input$Deck)
   })
+  output$results <- renderPrint({
+    ds1 <- values$df
+    a <- predict(modelFit, newdata = data.frame(ds1))
+    names(a) <- NULL
+    cat(a)
+  })
+})
--- titanic2/ui.R
+++ titanic2/ui.R
@@ -1,25 +1,11 @@
-library(mice)
-library(dplyr)
-library(randomForest)
-library(caret)
-setwd('c:/Kaggle/titanic')
-#Combine train.csv and test.csv files
-train <- read.csv('train.csv', na.strings = c("", "NA"), stringsAsFactors = F)
-test <- read.csv('test.csv', na.strings = c("", "NA"), stringsAsFactors = F)
-total <- bind_rows(train, test)
-
 library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("Titanic Survival Calculator"),
   sidebarPanel(
     p("Select person attributes to calculate his/her chances of surviving of the titanic sinking."),
-    sliderInput(
-      inputId = "Pclass",
-      label = h3("Ticket class:"),
-      min = 1,
-      max = 3,
-      step = 1,
-      value = 2),
+    radioButtons("Pclass", label = h3("Ticket class:"),
+                 choices = list("1st" = "1", "2nd" = "2", "3rd" = "3"), 
+                 selected = "2"),
     radioButtons("Sex", label = h3("Sex:"),
                  choices = list("male" = "male", "female" = "female"), 
                  selected = "female"),
@@ -52,7 +38,7 @@ shinyUI(pageWithSidebar(
       step = 0.5,
       value = 20),
     radioButtons("Embarked", label = h3("Port of Embarkation:"),
-                 choices = list("C" = "C", "Q" = "Q", "S" = "S"), 
+                 choices = list("Cherbourg" = "C", "Queenstown" = "Q", "Southampton" = "S"), 
                  selected = "Q"),
     radioButtons("Title", label = h3("Title:"),
                  choices = list("Mr" = "Mr", "Miss" = "Miss", "Mrs" = "Mrs","Master" = "Master", "Rare Title" = "Rare Title"), 
@@ -65,11 +51,11 @@ shinyUI(pageWithSidebar(
       step = 1,
       value = 2),
     radioButtons("Deck", label = h3("Deck:"),
-                 choices = list("C" = "C", "F" = "F", "E" = "E","B" = "B", "D" = "D", "A" = "A","G" = "G","T" = "T"), 
+                 choices = list("A" = "A","B" = "B","C" = "C", "D" = "D","E" = "E","F" = "F", "G" = "G","T" = "T"), 
                  selected = "C")
   ),
   mainPanel(
     h3("Survival Probability:"),
-    h4(textOutput('prob')),
+    h2(verbatimTextOutput("results")),
     p("Please note that this is estimated probability based on a logistic regression model."),
     p("That means this value is slightly different than historical survival rate."))))
